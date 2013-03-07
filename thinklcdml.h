cfbc26f2c46a9f53d1c45e67ddaafc367857d20e
Chris Perivolaropoulos <darksaga2006@gmail.com>
2013-03-07 19:18:11 +0200

    new base

--- thinklcdml.h
+++ thinklcdml.h
@@ -5,7 +5,7 @@
 #define TLCDML_MAJOR_NUMBER 0
 
 // ThinkLCD hardware constants
-#define TLCD_PHYSICAL_BASE	0x30000000	///< Memory mapped IO base address
+#define TLCD_PHYSICAL_BASE	0x6c000000	///< Memory mapped IO base address
 #define TLCD_MMIOALLOC		0x1000		///< Register file allocation length in bytes
 #define TLCD_PALETTE_OFFSET	0x400		///< Palette offset in bytes
 #define TLCD_PALETTE_COLORS	256		///< Number of palette colors
