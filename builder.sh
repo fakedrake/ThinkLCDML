5824c0789886170a4facfc88391907a90677f093
Yannis Economou <yeconomou@orange.(none)>
2013-12-18 16:49:16 +0200

    minor

--- builder.sh
+++ builder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH=$PATH:/home/filippakoc/Projects/xilinx-zynq-bootstrap-master/GNU_Tools/bin
+# export PATH=$PATH:/home/filippakoc/Projects/xilinx-zynq-bootstrap-master/GNU_Tools/bin
 make ARCH=arm CROSS_COMPILE=arm-xilinx-linux-gnueabi-
 
 
